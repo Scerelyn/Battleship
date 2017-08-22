8cafcfabac5914be6e0df323f7b92477d90fbcdb
Scerelyn <Scerelyn@gmail.com>
2017-08-22 16:56:09 -0600

    Add bare bones appearance, Ship class, and TileState enum

---
+++ Models/Ship.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip.Models
+{
+    public class Ship
+    {
+        public int Length { get; private set; }
+        public string Name { get; private set; }
+        public bool IsVertical { get; set; }
+    }
+}
---
+++ Models/TileState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip.Models
+{
+    public enum TileState
+    {
+        Normal,Missed,Hit,ShipHere
+    }
+}
